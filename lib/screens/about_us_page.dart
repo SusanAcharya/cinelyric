76a2ff34e2a9c9101b3e8456ef54ed7530d738a5
SusanAcharya <58133045+SusanAcharya@users.noreply.github.com>
2024-02-24 22:19:24 +0545

    Update about_us_page.dart

--- lib/screens/about_us_page.dart
+++ lib/screens/about_us_page.dart
@@ -1,4 +1,3 @@
-// about_us_page.dart
 import 'package:cinelyric/elements/appbar.dart';
 import 'package:cinelyric/elements/bottombar.dart';
 import 'package:flutter/material.dart';
@@ -8,42 +7,54 @@ class AboutUsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MyAppBar(),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'About CineLyric',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'CineLyric is a mobile app that provides users with information about movies, including reviews, ratings, and showtimes. Our goal is to make it easy for users to discover new movies and enjoy a seamless movie-watching experience.',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Key Features',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            ListTile(
-              leading: Icon(Icons.star),
-              title: Text('Movie Reviews and Ratings'),
-            ),
-            ListTile(
-              leading: Icon(Icons.schedule),
-              title: Text('Showtimes and Ticket Booking'),
-            ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('User Account and History'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildSection('About CineLyric',
+                  'The project is named "CineLyric". It was developed as part of academic project for the final year\'s project for the Bsc.CSIT course. CineLyric is a mobile application developed with Flutter that implements the speech-to-text technology which takes user speech as input and converts it to text and displays it back and then navigates the user to view the results of their input query. The app has two categories: movies and music. The movie category allows user to search for iconic movie lines while the Music category allows the user to search for popular music lyrics.'),
+              _buildDivider(),
+              _buildSection('How it Works',
+                  'CineLyric allows users to input movie dialogues or music lyrics as speech input and displays the spoken words on the screen. It then shows a list of movies or music that best match the user query based on popularity.'),
+              _buildDivider(),
+              _buildSection('Features',
+                  'Users can view details of movies such as name, genre, release date, ratings, and poster. For music, details include name, genre, artist name, release date, and lyrics. Users can also watch trailers for movies or music videos for songs.'),
+              _buildDivider(),
+              _buildSection('Contact the Team',
+                  'For any inquiries or feedback, please contact us at contact@cinelyric.com.'),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: MyAppBottomBar(currentPageIndex: 3),
+    );
+  }
+
+  Widget _buildSection(String title, String description) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        Text(
+          description,
+          style: TextStyle(fontSize: 16),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+
+  Widget _buildDivider() {
+    return Divider(
+      thickness: 5,
+      color: Colors.grey,
     );
   }
 }
