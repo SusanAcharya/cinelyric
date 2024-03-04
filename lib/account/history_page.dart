0bdb4dbb30370b1c63fe361a80e32def55b73d57
SusanAcharya <58133045+SusanAcharya@users.noreply.github.com>
2024-03-04 12:09:15 +0545

    Update history_page.dart

--- lib/account/history_page.dart
+++ lib/account/history_page.dart
@@ -1,13 +1,10 @@
 // history_page.dart
 import 'dart:convert';
-import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:cinelyric/elements/appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../elements/bottombar.dart';
-import '../screens/provider/movie_provider.dart';
 import '../screens/music_result_display.dart';
 import '../screens/movie_result_display.dart';
 
@@ -24,10 +21,12 @@ class _HistoryPageState extends State<HistoryPage> {
       []; // List to store search history data
   String _wordsSpoken = "";
   String type = "";
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
+    _scrollController.addListener(() {});
     getDataFromSharedPreferences().then((_) {
       fetchData();
     });
@@ -97,47 +96,56 @@ class _HistoryPageState extends State<HistoryPage> {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(10.0),
-              child: ListView.builder(
-                reverse: true,
-                itemCount: searchHistory.length, //search history length
-                itemBuilder: (context, index) {
-                  final historyItem = searchHistory.toList()[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      child: ListTile(
-                        title: Text('Query: ${historyItem['user_query']}'),
-                        subtitle:
-                            Text('Search Type: ${historyItem['search_type']}'),
-                        onTap: () {
-                          _wordsSpoken = historyItem['user_query'];
-                          type = historyItem['search_type'];
-                          //getMovie();
-                          if (type == 'movie') {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      MovieResult(query: _wordsSpoken)),
-                            );
-                          } else {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      MusicResult(query: _wordsSpoken)),
-                            );
-                          }
-                        },
-                      ),
-                    ),
-                  );
-                },
-              ),
+              child: searchHistory.isNotEmpty
+                  ? ListView.builder(
+                      controller: _scrollController,
+                      reverse: true,
+                      itemCount: searchHistory.length,
+                      itemBuilder: (context, index) {
+                        final historyItem = searchHistory.toList()[index];
+                        // Set different colors based on search type
+                        Color cardColor = historyItem['search_type'] == 'movie'
+                            ? Color.fromARGB(255, 33, 77, 35)
+                            : const Color.fromARGB(255, 49, 66, 75);
+
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.grey),
+                              borderRadius: BorderRadius.circular(8.0),
+                              color: cardColor,
+                            ),
+                            child: ListTile(
+                              title:
+                                  Text('Query: ${historyItem['user_query']}'),
+                              subtitle: Text(
+                                  'Search Type: ${historyItem['search_type']}'),
+                              onTap: () {
+                                _wordsSpoken = historyItem['user_query'];
+                                type = historyItem['search_type'];
+                                if (type == 'movie') {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            MovieResult(query: _wordsSpoken)),
+                                  );
+                                } else {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            MusicResult(query: _wordsSpoken)),
+                                  );
+                                }
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    )
+                  : Center(child: CircularProgressIndicator()),
             ),
           ),
         ],
